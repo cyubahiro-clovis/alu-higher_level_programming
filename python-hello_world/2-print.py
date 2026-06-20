36c73ae683a18b33fda11b1c3205167d6ed7763e
cyubahiro-clovis <m.clovis@alustudent.com>
2026-06-20 09:16:57 -0700

    Fixing PEP8 in printfunction

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 # This script print programming is like building a multilingual puzzle
-print ('"Programming is like building a multilingual puzzle')
+print('"Programming is like building a multilingual puzzle')
