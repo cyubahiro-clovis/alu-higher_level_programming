5806a64fded650a20fe12cc19e9c741d0ac6a78f
cyubahiro-clovis <m.clovis@alustudent.com>
2026-06-27 12:20:49 -0700

    Add ByteCode -> Python #2

---
+++ python-if_else_loops_functions/102-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+magic_calculation = __import__('102-magic_calculation').magic_calculation
+
+print(magic_calculation(2, 4, 6))
+print(magic_calculation(8, 4, 6))
+print(magic_calculation(8, 4, 2))
