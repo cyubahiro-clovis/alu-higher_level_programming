9597b00f7ddfbaed6d3f57799c17430e15bb6485
cyubahiro-clovis <m.clovis@alustudent.com>
2026-07-05 08:20:16 -0700

    Fix correct function name

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_reserved_list_integer(my_list=[]):
-    """Prints all integers of a list in reserve order, one per line."""
+def print_reversed_list_integer(my_list=[]):
+    """Prints all integers of a list in reversed order, one per line."""
     for i in my_list[::-1]:
         print("{:d}".format(i))
