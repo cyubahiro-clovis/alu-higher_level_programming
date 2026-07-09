c10c01f2e125f524bbb2ecad0a241014e3ae5099
cyubahiro-clovis <m.clovis@alustudent.com>
2026-07-09 10:47:05 -0700

    Add integers division with debug

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Divides two integers; prints the result in the finally block."""
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result 
