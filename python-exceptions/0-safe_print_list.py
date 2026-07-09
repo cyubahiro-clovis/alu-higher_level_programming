75e84e63c98463995f12fbd38ed657c2465e37b0
cyubahiro-clovis <m.clovis@alustudent.com>
2026-07-09 04:06:13 -0700

    Add safe list printing

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """Prints x elements of a list, returns how many were printed."""
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
